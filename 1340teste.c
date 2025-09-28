57b305e7211421f0e9c369e2dda34607cbc235c4
PedroMoreira191 <pedro.moreira@unifesp.br>
2025-09-27 21:59:06 -0300

    Update 1340teste.c

--- 1340teste.c
+++ 1340teste.c
@@ -1,23 +1,63 @@
 #include <stdlib.h>
-int eh_pilha(int **celula, int n){
-    int i, j=n;
+int eh_pilha(int *entra, int *sai, int n){
+    int i, j=n/2;
     int u=n/2;
     for(i=0; i<u; i++){
-        if(celula[i][1]!=celula[j-1][1]) return 0;
+        if(entra[i] != sai[j-1]) return 0;
         else j--;
     }
     return 1;
 }
-int eh_fila(int **celula, int n){
-    int i, j=n/2;
-    int u=n/2;
+int eh_fila(int *entra, int *sai, int n){
+    int i, u=n/2;
     for(i=0; i<u; i++){
-        if(celula[i][1]!=celula[j][1]) return 0;
-        else j++;
+        if(entra[i] != sai[i]) return 0;
     }
     return 1;
 }
-int eh_fila_prioridade(){
+int eh_fila_prioridade(int  *entra , int *sai, int n, int g){
+    int i, j;
+    int *ordenado = (int *) malloc((n/2)*sizeof(int));
+    if(ordenado==NULL) return -1;
+    for(i=0; i<n/2; i++){
+        ordenado[i] = entra[i];
+    }
+    for(i=0; i<n/2-1; i++){
+        for(j=i+1; j<n/2; j++){
+            if(ordenado[i] < ordenado[j]){
+                int temp = ordenado[i];
+                ordenado[i] = ordenado[j];
+                ordenado[j] = temp;
+            }
+        }
+    }
+    for(i=0; i<g-1; i++){
+        if(sai[i]==ordenado[i]) continue;
+        else return 0;
+    }
+    free(ordenado);
+    return 1;
+}
+int entra_e_sai(int **celula, int n){
+    int j=0;
+    int *entra = (int *) malloc(n*sizeof(int));
+    int *sai = (int *) malloc(n*sizeof(int));
+    if(entra==NULL || sai==NULL) return -1;
+    for(int i=0; i<n; i++){
+        if(celula[i][0]==1) entra[i] = celula[i][1];
+        else sai[j++] = celula[i][1];
+    }
+    if(eh_pilha(entra, sai, n) && eh_fila(entra, sai, n) && eh_fila_prioridade(entra, sai, n, j)) printf("not sure\n");
+    else if(eh_pilha(entra, sai, n) && eh_fila(entra, sai, n)) printf("not sure\n");
+    else if(eh_pilha(entra, sai, n) && eh_fila_prioridade(entra, sai, n, j)) printf("not sure\n");
+    else if(eh_fila(entra, sai, n) && eh_fila_prioridade(entra, sai, n, j)) printf("not sure\n");
+    else if(eh_pilha(entra, sai, n)) printf("stack\n");
+    else if(eh_fila(entra, sai, n)) printf("queue\n");
+    else if(eh_fila_prioridade(entra, sai, n, j)) printf("priority queue\n");
+    else printf("impossible\n");
+    free(entra);
+    free(sai);
+    return 0;
 }
 int main(){
     int i, j, n;
@@ -33,11 +73,11 @@ int main(){
             scanf("%d", &celula[i][j]);
         }
     }
-    printf("%d", eh_pilha(celula, n));
-    printf("%d", eh_fila(celula, n));
+    entra_e_sai(celula, n);
     
     for(i=0; i<n; i++){
         free(celula[i]);
     }
     free(celula);
+    return 0;
 }
