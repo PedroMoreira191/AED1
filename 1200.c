1c919e30c17af2bf9d0e4a2fa6d08e19f57fd007
PedroMoreira191 <pedro.moreira@unifesp.br>
2025-11-18 09:15:20 -0300

    Update 1200.c

--- 1200.c
+++ 1200.c
@@ -12,51 +12,41 @@ typedef struct {
     No *raiz;
 } Arv;
 
-void esquerda(No *no,char valor){
+void esquerda(No *no, char valor){
     if(no->esq == NULL){
-        No *novo = (No*) malloc(sizeof(No));
+        No *novo = malloc(sizeof(No));
         novo->valor = valor;
-        novo->esq = NULL;
-        novo->dir = NULL;
+        novo->esq = novo->dir = NULL;
         no->esq = novo;
-    }
-    else{
-        if(valor < no->esq->valor){
+    } else {
+        if(valor < no->esq->valor)
             esquerda(no->esq, valor);
-        }
-        else{
+        else
             direita(no->esq, valor);
-        }
     }
 }
 
 void direita(No *no, char valor){
     if(no->dir == NULL){
-        No *novo = (No*) malloc(sizeof(No));
+        No *novo = malloc(sizeof(No));
         novo->valor = valor;
-        novo->esq = NULL;
-        novo->dir = NULL;
+        novo->esq = novo->dir = NULL;
         no->dir = novo;
-    }
-    else{
-        if(valor >= no->dir->valor){
+    } else {
+        if(valor >= no->dir->valor)
             direita(no->dir, valor);
-        }
-        else{
+        else
             esquerda(no->dir, valor);
-        }
     }
 }
 
 void insere(Arv *arv, char valor){
     if(arv->raiz == NULL){
-        No *novo = (No*) malloc(sizeof(No));
+        No *novo = malloc(sizeof(No));
         novo->valor = valor;
-        novo->esq = NULL;
-        novo->dir = NULL;
+        novo->esq = novo->dir = NULL;
         arv->raiz = novo;
-    }
-    else {
+    } else {
         if(valor < arv->raiz->valor)
             esquerda(arv->raiz, valor);
         else
@@ -66,35 +56,53 @@ void insere(Arv *arv, char valor){
 
 int busca(No *raiz, char letra){
     if(raiz == NULL) return 0;
-
-    if(raiz->valor == letra)
-        return 1;
-
+    if(raiz->valor == letra) return 1;
     return busca(raiz->esq, letra) || busca(raiz->dir, letra);
 }
 
-void in(No *raiz){
+void inAux(No *raiz, int *primeiro){
     if(raiz != NULL){
-        in(raiz->esq);
-        printf("%c ", raiz->valor);
-        in(raiz->dir);
+        inAux(raiz->esq, primeiro);
+        if(!(*primeiro)) printf(" ");
+        printf("%c", raiz->valor);
+        *primeiro = 0;
+        inAux(raiz->dir, primeiro);
+    }
+}
+
+void in(No *raiz){
+    int primeiro = 1;
+    inAux(raiz, &primeiro);
+}
+
+void preAux(No *raiz, int *primeiro){
+    if(raiz != NULL){
+        if(!(*primeiro)) printf(" ");
+        printf("%c", raiz->valor);
+        *primeiro = 0;
+        preAux(raiz->esq, primeiro);
+        preAux(raiz->dir, primeiro);
     }
 }
 
 void pre(No *raiz){
+    int primeiro = 1;
+    preAux(raiz, &primeiro);
+}
+
+void posAux(No *raiz, int *primeiro){
     if(raiz != NULL){
-        printf("%c ", raiz->valor);
-        pre(raiz->esq);
-        pre(raiz->dir);
+        posAux(raiz->esq, primeiro);
+        posAux(raiz->dir, primeiro);
+        if(!(*primeiro)) printf(" ");
+        printf("%c", raiz->valor);
+        *primeiro = 0;
     }
 }
 
 void pos(No *raiz){
-    if(raiz != NULL){
-        pos(raiz->esq);
-        pos(raiz->dir);
-        printf("%c ", raiz->valor);
-    }
+    int primeiro = 1;
+    posAux(raiz, &primeiro);
 }
 
 void libera(No *raiz){
@@ -104,10 +112,9 @@ void libera(No *raiz){
     free(raiz);
 }
 
-int main (){
+int main(){
     Arv T;
     T.raiz = NULL;
-
     char s[20], letra;
 
     while(scanf("%s", s) != EOF){
@@ -116,6 +123,7 @@ int main (){
             scanf(" %c", &letra);
             insere(&T, letra);
         }
+
         else if(strcmp(s, "P") == 0){
             scanf(" %c", &letra);
             if(busca(T.raiz, letra))
@@ -123,14 +131,17 @@ int main (){
             else
                 printf("%c nao existe\n", letra);
         }
+
         else if(strcmp(s, "INFIXA") == 0){
             in(T.raiz);
             printf("\n");
         }
+
         else if(strcmp(s, "PREFIXA") == 0){
             pre(T.raiz);
             printf("\n");
         }
+
         else if(strcmp(s, "POSFIXA") == 0){
             pos(T.raiz);
             printf("\n");
